3f24c6d75abc62a3ee37ad8be03ec147a3b73a4e
Marcelo Freitas <glacial@gmail.com>
2019-08-19 21:08:39 -0400

    Add logo styles and logic

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -13,6 +13,11 @@ const Header = props => {
         return element.slug === 'menu-topo';
     });
 
+    // Get Logo
+    const navLogo = menuItems.find(function(element) {
+        return element.slug === 'menu-logo';
+    });
+
     // Get Telephone
     const navTelephone = menuItems.find(function(element) {
         return element.slug === 'telephone';
@@ -27,6 +32,18 @@ const Header = props => {
     const navMenuItem = menuItems.map(navMenuItem => {
         const { content, id } = navMenuItem;
 
+        if (id === navLogo.id) {
+            return (
+                <div className='site-logo' key='site-logo'>
+                    <Link href='/'>
+                        <a className='site-info__link'>
+                            <div className='site-logo__img' dangerouslySetInnerHTML={{ __html: content.rendered }} key={id} />
+                        </a>
+                    </Link>
+                </div>
+            );
+        }
+
         if (id === navID.id) {
             return <div className='menu-top' dangerouslySetInnerHTML={{ __html: content.rendered }} key={id} />;
         }
@@ -34,6 +51,7 @@ const Header = props => {
         if (id === navTelephone.id) {
             return <div className='menu-telephone' dangerouslySetInnerHTML={{ __html: content.rendered }} key={id} />;
         }
+
         if (id === navLanguage.id) {
             return <div className='menu-language' dangerouslySetInnerHTML={{ __html: content.rendered }} key={id} />;
         }
@@ -45,13 +63,6 @@ const Header = props => {
                 <div className='site-info'>
                     <h1 className='site-info__title'>{siteName}</h1>
                     <p className='site-info__description'>{siteDescription}</p>
-                </div>
-                <div className='site-logo'>
-                    <Link href='/'>
-                        <a className='site-info__link'>
-                            <img src='https://brincaottawa.files.wordpress.com/2019/08/logofinal-01402x.png' alt='logo' className='site-logo__img' />
-                        </a>
-                    </Link>
                 </div>
                 <div className='site-header__menu'>{navMenuItem}</div>
             </header>
