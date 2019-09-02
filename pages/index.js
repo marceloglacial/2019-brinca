59f5a553ca1487d87edb16efe9c4514b37c4db7e
Marcelo Freitas <glacial@gmail.com>
2019-09-02 12:44:46 -0400

    Improve api url

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,20 @@ import Footer from '../components/Footer/Footer';
 import fetch from 'isomorphic-unfetch';
 
 IndexPage.getInitialProps = async function() {
-    const siteRes = await fetch('https://public-api.wordpress.com/rest/v1/sites/brincanada.wordpress.com/');
+    const siteURl = `brincanada.wordpress.com`
+    const siteAPIv1 = `https://public-api.wordpress.com/rest/v1/sites/${siteURl}`
+    const siteAPIv2 = `https://public-api.wordpress.com/wp/v2/sites/${siteURl}`
+
+    const siteRes = await fetch(siteAPIv1);
     const siteData = await siteRes.json();
 
-    const pagesRes = await fetch('https://public-api.wordpress.com/wp/v2/sites/brincanada.wordpress.com/pages/');
+    const pagesRes = await fetch(`${siteAPIv2}/pages/`);
     const pagesData = await pagesRes.json();
 
-    const catRes = await fetch('https://public-api.wordpress.com/wp/v2/sites/brincanada.wordpress.com/categories/');
+    const catRes = await fetch(`${siteAPIv2}/categories/`);
     const catData = await catRes.json();
 
-    const postsRes = await fetch('https://public-api.wordpress.com/wp/v2/sites/brincanada.wordpress.com/posts/');
+    const postsRes = await fetch(`${siteAPIv2}/posts/`);
     const postsData = await postsRes.json();
 
     return {
