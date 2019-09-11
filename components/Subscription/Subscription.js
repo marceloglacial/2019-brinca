a7295ad1d890e94324eac7bf2fcc9bbdfb3f0034
Marcelo Freitas <glacial@gmail.com>
2019-09-11 01:19:44 -0400

    add form

---
+++ components/Subscription/Subscription.js
@@ -0,0 +1,13 @@
+// import './ContactForm.scss';
+
+const Subscription = props => {
+    return (
+        <div className='subscription-form'>
+            <iframe src='https://docs.google.com/forms/d/e/1FAIpQLSfv4-p0GjPdydFmK8Fte7OD9rt6pM3WYlA8FFATfw-5bNH4QQ/viewform?embedded=true' width='640' height='740' frameborder='0' marginheight='0' marginwidth='0'>
+                Loading…
+            </iframe>
+        </div>
+    );
+};
+
+export default Subscription;
