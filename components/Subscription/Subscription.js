e512baeec54a81cb626d6e42f907c33cc7f41a7d
Marcelo Freitas <glacial@gmail.com>
2019-09-11 01:24:49 -0400

    add form to global settings

--- components/Subscription/Subscription.js
+++ components/Subscription/Subscription.js
@@ -1,9 +1,9 @@
-// import './ContactForm.scss';
+import siteConfig from '../Global/Global';
 
 const Subscription = props => {
     return (
         <div className='subscription-form'>
-            <iframe src='https://docs.google.com/forms/d/e/1FAIpQLSfv4-p0GjPdydFmK8Fte7OD9rt6pM3WYlA8FFATfw-5bNH4QQ/viewform?embedded=true' width='640' height='740' frameborder='0' marginheight='0' marginwidth='0'>
+            <iframe src={`${siteConfig.subscription}/viewform?embedded=true`} width='640' height='740' frameborder='0' marginheight='0' marginwidth='0'>
                 Loading…
             </iframe>
         </div>
