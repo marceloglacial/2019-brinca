30aefc5fdfd9edfbdcefe8a1a25edbd4c51a0baa
Marcelo Freitas <glacial@gmail.com>
2019-09-16 00:41:12 -0400

    Fix menu

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -5,15 +5,19 @@ import Link from 'next/link';
 import '../../node_modules/bootstrap/scss/bootstrap.scss';
 import './Nav.scss';
 
+import { useState } from 'react';
+
 const Nav = props => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
     const nav = props.data;
 
     const navitens = (
-        <ul className='site-nav--header__items container'>
+        <ul className={`site-nav--header__items container ${isOpen ? 'is-open' : ''}`}>
             {nav.map(post => (
                 <li className='site-nav--header__item' key={post.id}>
                     <Link href='[id]' as={`${post.slug}`}>
-                        <a>{post.title.rendered}</a>
+                        <a onClick={toggleMenu}>{post.title.rendered}</a>
                     </Link>
                 </li>
             ))}
@@ -25,7 +29,10 @@ const Nav = props => {
             <Head>
                 <link rel='shortcut icon' type='image/x-icon' href={siteConfig.favicon} />
             </Head>
-            <nav className='site-nav site-nav--header'>{navitens}</nav>
+            <nav className={`site-nav site-nav--header`}>
+                <button className={`site-nav site-nav-button ${isOpen ? 'is-open' : ''}`} onClick={toggleMenu} />
+                {navitens}
+            </nav>
         </>
     );
 };
