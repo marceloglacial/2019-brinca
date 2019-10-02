e8419889e72ba6ba547a8c10822580b86d6b855a
Marcelo Freitas <glacial@gmail.com>
2019-10-01 22:39:48 -0400

    Add responsive mixins

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import Nav from '../Nav/Nav';
-import Telephone from '../Telephone/Telephone';
 import './Header.scss';
 
 const Header = props => (
@@ -9,9 +8,6 @@ const Header = props => (
         <section className='site-header'>
             <Logo />
             <Nav {...props} />
-            <div className='site-header__telephone'>
-                <Telephone />
-            </div>
         </section>
     </header>
 );
