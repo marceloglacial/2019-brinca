97846f0b992df1c4eacae3adae79810d9b43a09e
Marcelo Freitas <glacial@gmail.com>
2019-11-27 19:27:28 -0500

    Filter footer link types

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,16 +1,55 @@
 import siteConfig from '../../components/Global/Global';
+import Link from 'next/link';
 import './Footer.scss';
 
 const Footer = props => {
     const { title } = siteConfig;
     const year = new Date().getFullYear();
-    const footerData = props.footer;
+    const language = props.language ? props.language : '';
+    const footerData = {
+        portugues: [
+            {
+                text: 'Associe-se',
+                link: 'associe-se'
+            },
+            {
+                text: 'Facebook',
+                link: 'http://facebook.com'
+            }
+        ]
+    };
 
-    const footerContent = <div className={`site-footer-nav__items container`} dangerouslySetInnerHTML={{ __html: footerData.content.rendered }} />;
+    const footerItems = (
+        <>
+            {footerData.portugues.map(item => {
+                const isExternal = item.link.includes('http', 'www', '.com');
+
+                if (isExternal) {
+                    return (
+                        <li key={item.link}>
+                            <a href={item.link} target='_blank'>
+                                {item.text}
+                            </a>
+                        </li>
+                    );
+                }
+
+                return (
+                    <li key={item.link}>
+                        <Link href={`${language}/[id]`} as={`${language}/${item.link}`}>
+                            <a>{item.text}</a>
+                        </Link>
+                    </li>
+                );
+            })}
+        </>
+    );
 
     return (
         <footer>
-            <section className='site-footer-nav'>{footerContent}</section>
+            <section className='site-footer-nav'>
+                <ul className='site-footer-nav__items'>{footerItems}</ul>
+            </section>
             <section className='site-footer-copyrights'>
                 <p>
                     Copyrights {title} - {year}
